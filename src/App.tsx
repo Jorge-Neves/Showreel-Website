18c1bdffa7764c64b1eba9f6bfe1dab3742ede0b
Paulo Santos <paulo.jorge.ngs@gmail.com>
2023-11-05 20:46:11 +0000

    Add About Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import About from './pages/About';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <About />
     </div>
   );
 }
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla iure
+        nemo aspernatur! Accusantium, velit fugiat incidunt rem aspernatur
+        tempora magnam recusandae expedita quam delectus, obcaecati nulla libero
+        debitis, facere similique!
+      </p>
+    </>
+  );
+};
+
+export default About;
